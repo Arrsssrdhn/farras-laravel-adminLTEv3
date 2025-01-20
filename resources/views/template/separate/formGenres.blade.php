4b1ea5ae98d39e478ed31a0de038a2d206b20a71
Arrsssrdhn <arrasucup@gmail.com>
2025-01-20 19:35:58 +0700

    farras-laravel-adminLTEv3

---
+++ resources/views/template/separate/formGenres.blade.php
@@ -0,0 +1,114 @@
+
+
+<section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Genre</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form>
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Jenis Genre</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="    ">
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">id Genre</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="    ">
+                </div>
+
+              <!-- /.card-body -->
+  
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+
+            
+          </div>
+          <!-- /.card -->
+  
+        
+  
+        </div>
+        <!--/.col (left) -->
+        <!-- right column -->
+       
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+
+
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Genre</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form>
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">nama film</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="    ">
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">id film</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="    ">
+                </div>
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">genre film</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="  ">
+                </div>
+
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">tanggal rilis</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="  ">
+                </div>
+
+
+                <div class="form-group">
+                  <label for="exampleInputEmail1">peran</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder=" ">
+                </div>
+
+
+              <!-- /.card-body -->
+  
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+
+            
+          </div>
+          <!-- /.card -->
+  
+        
+  
+        </div>
+        <!--/.col (left) -->
+        <!-- right column -->
+       
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\BukuTamucontroller;
+
+// Route::get('/well', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/welltosign', function () {
+//     return view('welcomeToSignin');
+// });
+
+// Route::get('/signin', function () {
+//     return view('signin');
+// });
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/', [BukuTamuController::class, 'welcome']);
+route::get('/signin', [BukuTamucontroller::class, 'signin']);
+route::get('/selesai', [BukuTamucontroller::class, 'selesai']);
+route::get('tem/master', [BukuTamucontroller::class, 'master']);
+route::get('/index', [BukuTamucontroller::class, 'index']);
+route::get('/sign', [BukuTamucontroller::class, 'welcomeToSignin']);
+
+
